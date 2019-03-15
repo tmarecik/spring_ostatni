bee7595b1a84771f8c2161d58b5b1cd206c1f535
Mateusz Wręczycki <setsudan.hana@gmail.com>
2019-03-16 00:51:20 +0100

    initial commit

---
+++ src/main/java/pl/edu/wszib/todolist/springtodolist/SpringTodoListApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.todolist.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoListApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/edu/wszib/todolist/springtodolist/configuration/DozerConfiguration.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.todolist.springtodolist.configuration;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DozerConfiguration {
+
+    @Bean
+    public Mapper mapper() {
+        return new DozerBeanMapper();
+    }
+
+}
